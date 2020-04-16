0030300b768de8400b5deab9c2f6c729b1d35c94
CompleteCloudGuru <32646665+completecloudguru@users.noreply.github.com>
2020-04-16 07:10:04 +0100

    added MVC movie

---
+++ MvcMovie/MvcMovie/Data/MvcMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext(DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
